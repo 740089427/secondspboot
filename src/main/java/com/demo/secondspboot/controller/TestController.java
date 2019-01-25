bf98149b46a75295e607572171baf37a89136659
bingguang <bingguang@outlook.com>
2019-01-25 14:00:22 +0800

    fix swagger

--- src/main/java/com/demo/secondspboot/controller/TestController.java
+++ src/main/java/com/demo/secondspboot/controller/TestController.java
@@ -2,10 +2,9 @@ package com.demo.secondspboot.controller;
 
 import com.demo.secondspboot.model.TestModel;
 import com.demo.secondspboot.service.TestService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +15,19 @@ public class TestController {
     private TestService testService;
 
     @RequestMapping(value = "/getAlltest")
+
+
    public List<TestModel> getAlltest(){
        return testService.getAll();
    }
-    @RequestMapping(value = "/getModelById")
-    public List<TestModel> getModelById(int id){
+    @ApiOperation(value = "查询用户" , httpMethod ="GET", notes = "根据Id查询用户")
+    @ApiImplicitParams(@ApiImplicitParam(  name = "id",value = "用户ID",required = true,dataType = "int",paramType = "query"))
+    @ApiResponse(response=TestModel.class, code = 200,  message = "接口返回对象参数")
+    @ResponseBody
+    @RequestMapping(value = "/getModelById",method=RequestMethod.GET)
+    public List<TestModel> getModelById( int id){
         return testService.getModelById(id);
     }
+
+
 }
